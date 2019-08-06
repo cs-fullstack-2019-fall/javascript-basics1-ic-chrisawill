846f291eaa7f98970b2e226601e70b9d3a81d222
Christian Williams <chrisawill05@gmail.com>
2019-08-06 15:36:55 -0500

    classwork from javascript excersise

---
+++ index.js
@@ -0,0 +1,6 @@
+//Problem 1 Print "Hello World"
+
+//Probelm 2 Create a variable named schoolName. Print "My school's name is schoolName"
+alert("Hello World");
+var schoolName = "Code Crew";
+ alert("My school's name is " + schoolName);
